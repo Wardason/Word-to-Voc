9288d5f1b001bad292393b3aafa0011ad1dd498e
Leonard <66140015+Wardason@users.noreply.github.com>
2023-07-20 20:34:22 +0200

    basic html structure and post function

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+        return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    input_value = request.form['text']
+    print(input_value)
     return render_template('index.html')
 
 
