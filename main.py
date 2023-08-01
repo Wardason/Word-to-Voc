b77c6b0c2dcbf20a6ce213b6b504e8d0be96f82f
Leonard <66140015+Wardason@users.noreply.github.com>
2023-08-01 20:56:54 +0200

    error handeling

--- main.py
+++ main.py
@@ -45,8 +45,9 @@ def submit():
         translated_words.append(word_translator(word).text)
         try:
             example_sentence.append(sentence_creator(word))
-        except:
-            print("error motherfucker")
+            print(example_sentence)
+        except Exception as e:
+            return render_template('error.html', error=e)
 
     combined_lists = dict(zip(input_value, translated_words))
     for index, key in enumerate(input_value):
