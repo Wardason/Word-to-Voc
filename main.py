8efca8664be40de10caa611e89ea81ac31d88926
Leonard <66140015+Wardason@users.noreply.github.com>
2023-08-13 15:15:53 +0200

    clean code refactoring

--- main.py
+++ main.py
@@ -5,15 +5,13 @@ import os
 from googletrans import Translator
 import openai
 
-# This function takes an English word as input and translates it to German and retruns it
+
+# region Functions
 def word_translator(word):
     translator = Translator()
     translation = translator.translate(word, src='en', dest='de')
     return translation
 
-
-# This function uses the ChatGPT API to generate a random sentence, incorporating the provided English Keyword.
-# The generated sentence is returned as the output.
 def sentence_creator(keyword):
     openai.api_key = api_key
     completion = openai.ChatCompletion.create(
@@ -27,9 +25,49 @@ def sentence_creator(keyword):
     )
     return completion.choices[0].message.content
 
-# This function loads the env file, in which the api key and the session key is stored
 def configure():
     load_dotenv()
+
+def save_data():
+    combined_lists = session.get('combined_lists', {})
+    with open('voc-data.csv', mode='a', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        for word, translation in combined_lists.items():
+            writer.writerow([word + "   - " + translation])
+    return combined_lists
+
+def show_data():
+    list_of_rows = []
+    with open('voc-data.csv', newline='', encoding='utf-8') as csv_file:
+        csv_data = csv.reader(csv_file, delimiter='/')
+        list_of_rows = [row for row in csv_data]
+        print(list_of_rows)
+    return list_of_rows
+
+def sorting_word_translations():
+    translated_words = []
+    input_value = request.form['text'].lower().split()
+
+    for word in input_value:
+        creating_word_translation = word_translator(word).text
+        translated_words.append(creating_word_translation)
+    return translated_words
+
+def sorting_sentence_translations():
+    example_sentence = []
+    input_value = request.form['text'].lower().split()
+
+    for word in input_value:
+        creating_sentence_translation = sentence_creator(word)
+        example_sentence.append(creating_sentence_translation)
+    return example_sentence
+
+def merge_translations_and_context(input_value, translated_words, example_sentence):
+    combined_lists = dict(zip(input_value, translated_words))
+    for index, key in enumerate(input_value):
+        combined_lists[key] = combined_lists[key] + '   - ' + example_sentence[index]
+    return combined_lists
+# endregion
 
 
 api_key = os.getenv('api_key')
@@ -38,60 +76,29 @@ app_secret = os.getenv('app_secret')
 app = Flask(__name__)
 app.secret_key = app_secret
 
-# Main website
 @app.route("/")
 def index():
     return render_template('index.html')
 
-# Submit website
-# The function performs the following steps:
-#   - It retrieves the form input data as a list of words
-#   - For each word in the input list, it translates the word and creates an example sentence
-#   - If there's an exception during sentence creation, it renders the error website
-#   - The translated words and example sentences are combined into a dictionary and stored in the session variable.
 @app.route('/submit', methods=['POST'])
 def submit():
-    translated_words = []
-    example_sentence = []
     input_value = request.form['text'].lower().split()
+    translated_words = sorting_word_translations()
+    example_sentence = sorting_sentence_translations()
 
-    for word in input_value:
-        translated_words.append(word_translator(word).text)
-        try:
-            example_sentence.append(sentence_creator(word))
-        except Exception as e:
-            print(e)
-            return render_template('error.html')
-
-    combined_lists = dict(zip(input_value, translated_words))
-    for index, key in enumerate(input_value):
-        combined_lists[key] = combined_lists[key] + '   - ' + example_sentence[index]
-
+    combined_lists = merge_translations_and_context(input_value, translated_words, example_sentence)
     session['combined_lists'] = combined_lists
 
     return render_template('translated.html', words=combined_lists)
 
-# This function is triggered when the user pushes the save button to store the data in a CSV file (database). It
-# retrieves the combined list data from the session, which contains word-translation pairs, and appends the data to a
-# CSV file named "voc-data.csv"
 @app.route('/save', methods=['POST'])
-def save_data():
-    combined_lists = session.get('combined_lists', {})
-    with open('voc-data.csv', mode='a', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        for word, translation in combined_lists.items():
-            writer.writerow([word + "   - " + translation])
+def save_data_website():
+    combined_lists = save_data()
     return render_template('translated.html', words=combined_lists)
 
-# This function shows the saved vocs history
-# It reads the data from the CSV file and stores the rows in a list
 @app.route('/show', methods=['POST'])
-def show_data():
-    list_of_rows = []
-    with open('voc-data.csv', newline='', encoding='utf-8') as csv_file:
-        csv_data = csv.reader(csv_file, delimiter='/')
-        list_of_rows = [row for row in csv_data]
-        print(list_of_rows)
+def show_data_website():
+    list_of_rows = show_data()
     return render_template('show-data.html', data=list_of_rows)
 
 
